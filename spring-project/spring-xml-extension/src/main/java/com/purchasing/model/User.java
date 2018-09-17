961012628db360fd7f43b5ddb16220c8c9056e4a
zhangwei <15711366039@163.com>
2018-09-18 00:09:06 +0800

    add spring part code

---
+++ spring-project/spring-xml-extension/src/main/java/com/purchasing/model/User.java
@@ -0,0 +1,13 @@
+package com.purchasing.model;
+
+import lombok.Data;
+
+/**
+ * @author Administrator
+ * @createTime 2018/9/17
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+}
