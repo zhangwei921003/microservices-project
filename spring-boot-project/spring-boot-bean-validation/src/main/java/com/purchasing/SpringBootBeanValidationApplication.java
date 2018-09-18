2b289ce19e788f0c88cec6ed7ae21d8d5bd7b3ba
zhangwei <15711366039@163.com>
2018-09-18 16:27:51 +0800

    bean validation

---
+++ spring-boot-project/spring-boot-bean-validation/src/main/java/com/purchasing/SpringBootBeanValidationApplication.java
@@ -0,0 +1,12 @@
+package com.purchasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeanValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBeanValidationApplication.class, args);
+    }
+}
